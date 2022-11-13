1e6bd841b6f63c67f0624dc73d67c80597cc633d
Ojerry <onotu128@gmail.com>
2022-11-13 01:06:30 +0100

    sorted list of countries

--- app/src/main/java/com/ojinc/explorecountries/MainActivity.kt
+++ app/src/main/java/com/ojinc/explorecountries/MainActivity.kt
@@ -5,17 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.util.Log.d
+import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
+import androidx.appcompat.widget.SearchView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ojinc.explorecountries.data.CountryData
+import com.ojinc.explorecountries.data.CountryDataItem
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
+import kotlin.collections.ArrayList
 
 const val BASE_URL = "https://restcountries.com/v3.1/"
 class MainActivity : AppCompatActivity() {
@@ -23,6 +29,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var countryAdapter: CountryAdapter
     lateinit var linearLayoutManager: LinearLayoutManager
     lateinit var recyclerView: RecyclerView
+    lateinit var searchView: SearchView
+    lateinit var tempCountryData: CountryData
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,14 +40,23 @@ class MainActivity : AppCompatActivity() {
         linearLayoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = linearLayoutManager
 
-
-
-
         getMyData()
-//        val btn: Button = findViewById(R.id.btn)
-//        btn.setOnClickListener(){
-//            getMyData()
-//        }
+
+        searchView = findViewById(R.id.searchView)
+        searchView.clearFocus()
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+//                filterList(newText)
+                Log.d("pressed", "beep")
+
+                return true
+            }
+
+        })
     }
 
     private fun getMyData() {
@@ -52,11 +69,16 @@ class MainActivity : AppCompatActivity() {
         val retrofitData = retrofitBuilder.getData()
         retrofitData.enqueue(object : Callback<CountryData?> {
             override fun onResponse(call: Call<CountryData?>, response: Response<CountryData?>) {
-                val responseBody = response.body()!!
-//                Log.e("data", responseBody.toString())
-                countryAdapter = CountryAdapter(baseContext, responseBody)
+                var responseBody = response.body()!!
+                tempCountryData = CountryData()
+                for(item in responseBody.sortedBy { it.name.common }){
+                    tempCountryData.add(item)
+                }
+                countryAdapter = CountryAdapter(baseContext, tempCountryData)
                 countryAdapter.notifyDataSetChanged()
                 recyclerView.adapter = countryAdapter
+
+
 
                 countryAdapter.setOnItemClickListener(object : CountryAdapter.onItemClickListener{
                     override fun onItemClick(position: Int) {
@@ -64,15 +86,21 @@ class MainActivity : AppCompatActivity() {
 //                        myStringBuilder.append(responseBody[position].name.common)
                         val intent = Intent(this@MainActivity, DetailActivity::class.java)
 
-                        intent.putExtra("name", responseBody[position].name.common)
-                        intent.putExtra("population", responseBody[position].population.toString())
-                        intent.putExtra("flag", responseBody[position].flags.png)
-                        intent.putExtra("coatOfArms", responseBody[position].coatOfArms.png)
+//                        intent.putExtra("name", responseBody[position].name.common)
+//                        intent.putExtra("population", responseBody[position].population.toString())
+//                        intent.putExtra("flag", responseBody[position].flags.png)
+//                        intent.putExtra("coatOfArms", responseBody[position].coatOfArms.png)
+                        intent.putExtra("name", tempCountryData[position].name.common)
+                        intent.putExtra("population", tempCountryData[position].population.toString())
+                        intent.putExtra("flag", tempCountryData[position].flags.png)
+                        intent.putExtra("coatOfArms", tempCountryData[position].coatOfArms.png)
 //                        intent.putExtra("currencies", responseBody[position].currencies)
                         startActivity(intent)
                     }
 
                 })
+
+
             }
 
             override fun onFailure(call: Call<CountryData?>, t: Throwable) {
@@ -84,3 +112,22 @@ class MainActivity : AppCompatActivity() {
 
 
 }
+
+
+
+
+//tempArrayData.clear()
+//Log.d("check: ", tempArrayData.toString())
+//val searchText = newText!!.toLowerCase(Locale.getDefault())
+//if (searchText.isNotEmpty()){
+//    tempArrayData.forEach{
+//        if (it.name.common.toLowerCase(Locale.getDefault()).contains(searchText)){
+//            tempArrayData.add(it)
+//        }
+//    }
+//    recyclerView.adapter!!.notifyDataSetChanged()
+//} else{
+//    tempArrayData.clear()
+//    tempArrayData.addAll(buff)
+//    recyclerView.adapter!!.notifyDataSetChanged()
+//}
